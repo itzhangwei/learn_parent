011b1625912dcce217f9de2f8b8f6275582240ab
zhangwei <mr_zhangwei1995@163.com>
2017-08-17 13:18:55 +0800

    redis队列

--- learn_redis/src/test/java/com/learn/queue/TestQueue.java
+++ learn_redis/src/test/java/com/learn/queue/TestQueue.java
@@ -1,8 +1,22 @@
 package com.learn.queue;
 
+import org.junit.Test;
+import redis.clients.jedis.Jedis;
+
 /**
  * 测试redis的队列功能
  */
 public class TestQueue {
 
+    private Jedis jedis = new Jedis("localhost");
+
+    @Test
+    public void queueTest() {
+        //同时向redis的队列中添加多个值
+        jedis.lpush("list1", "this is a fist value in this list", "2");
+        System.out.println(jedis.llen("list1"));
+        String value = jedis.rpop("list1");
+        System.out.println(value);
+        jedis.close();
+    }
 }
