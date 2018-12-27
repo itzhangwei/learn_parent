8275e58a0942c192072bac5b78a8031e9a0ad819
Zhangwei <zhangwei>
2018-12-27 18:20:46 +0800

    submitter:张伟 comment：自定义easyOpen返回值修改

--- learn_easyopen/src/main/java/com/learn/DriverLearnEasy.java
+++ learn_easyopen/src/main/java/com/learn/DriverLearnEasy.java
@@ -1,9 +1,13 @@
 package com.learn;
 
+import com.gitee.easyopen.ApiConfig;
+import com.learn.easy.open.config.result.MyResultCreator;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author 张伟
@@ -18,7 +22,36 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @EnableEncryptableProperties
 @MapperScan(value = "com.learn.mapper.*")
 public class DriverLearnEasy {
+
     public static void main(String[] args) {
         SpringApplication.run(DriverLearnEasy.class, args);
+    }
+
+
+    /**
+     * 构造函数，注入该类下需要spring注入的一些依赖对象
+     * @param apiConfig easy open的核心配置对象
+     */
+    @Autowired
+    public DriverLearnEasy(ApiConfig apiConfig) {
+        this.apiConfig = apiConfig;
+    }
+
+    /*
+     * easy open的核心配置对象
+     */
+    private final ApiConfig apiConfig;
+    
+
+
+    /**
+     * 增强，使用自定义easyOpen配置
+     */
+    @Bean
+    public void strengthenApiConfig() {
+
+        //使用自定义返回类
+        apiConfig.setResultCreator(new MyResultCreator());
+
     }
 }
